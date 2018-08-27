88d8fd7fcb7837945d1eb1c8ee48b3583926ce1b
Ryhan Chowdhury <ryhanchowdhury@apples-MacBook-Pro-2.local>
2018-08-27 15:09:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+const logDriverNames = (drivers) => {
+  drivers.forEach( driver => {
+    console.log(driver.name)
+  })
+}
+
+const logDriversByHometown = (drivers, hometown ) => {
+  drivers.forEach ( driver => {
+    if (driver.hometown === hometown) {
+      console.log(driver.name)
+    }
+  });
+};
+
+const driversByRevenue = (drivers) => {
+  return drivers.slice().sort( (driverOne, driverTwo) => {
+    if(driverOne.revenue < driverTwo.revenue){
+      return -1
+    } else if (driverOne.revenue > driverTwo.revenue){
+      return 1
+    } else {
+      return 0
+    }
+  })
+}
+const driversByName = (drivers) => {
+  return drivers.slice().sort( (driverOne, driverTwo) => {
+    return driverOne.name.localeCompare(driverTwo.name)
+  })
+}
+
+const totalRevenue = (drivers) => {
+  return drivers.reduce( (total, currentDriver) => {
+    return currentDriver.revenue + total;
+  }, 0)
+}
+
+const averageRevenue = (drivers) => {
+  return totalRevenue(drivers) / drivers.length
+}
