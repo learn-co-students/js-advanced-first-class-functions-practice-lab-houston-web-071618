a1263c1de91f4197f21811bd818dc4714580780e
RebGov <becci.govert@gmail.com>
2018-08-31 16:59:15 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const logDriverNames = function (drivers) {
+  drivers.forEach(function(driver){
+    console.log(driver.name)
+  });
+};
+const logDriversByHometown = function (drivers, hometown) {
+  drivers.forEach(function (driver) {
+    if (driver.hometown === hometown) {
+      console.log(driver.name);
+    }
+  });
+};
+const driversByRevenue = function (drivers) {
+    // console.log(drivers) why use .slice()?
+    return drivers.slice().sort(function (driverA, driverB) {
+      return driverA.revenue - driverB.revenue;
+    });
+};
+const driversByName  = function(drivers) {
+  return drivers.slice().sort(function (driverA, driverB) {
+      return driverA.name.localeCompare(driverB.name);
+  });
+};
+const totalRevenue = function(drivers) {
+  return drivers.reduce(function (total, currentDriver){
+    return currentDriver.revenue + total;
+  }, 0); //initial value passed as second argument
+};
+
+const averageRevenue = function (drivers) {
+  return totalRevenue(drivers)/drivers.length
+};//use previous code as already have total
